797e7d7f24d72c1a40b5c0b02ae9ef51b62bb860
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-06-06 15:26:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = Client(
     "mo",
     api_id=21627756,
     api_hash="fe77fbf0cae9f7f5ece37659e2466cf1",
-    bot_token="6305475220:AAEK9B4q8y-zDIUn4hCrpp5a-l6C-JLt7jY",#توكن المصنع
+    bot_token="7197234381:AAEYoNfw5En91ItaBhFcPRlN3zKevqhwgl0",#توكن المصنع
     plugins=dict(root="Mody")
     )
 
